00191760f77a711304e0fdea9be6dc4e5bb192e6
koudaiii <cs006061@gmail.com>
2015-12-25 11:07:34 +0900

    Not Logentries

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-if Rails.env.production?
-  Rails.logger = Le.new(ENV["LOGENTRIES_TOKEN"], tag: true)
-end
